a73251fb2fdc661badc94e9eba7cb2d132a46d19
Kensuke Nagae <kyanny@gmail.com>
2021-01-27 01:16:15 +0900

    Add admin with inline, ManyToMany(through) to work

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Bookmark, Tag, Tag2Bookmark
+
+
+class Tag2BookmarkInline(admin.TabularInline):
+    model = Tag2Bookmark
+
+
+class BookmarkAdmin(admin.ModelAdmin):
+    inlines = (Tag2BookmarkInline,)
+
+
+class TagAdmin(admin.ModelAdmin):
+    inlines = (Tag2BookmarkInline,)
+
+
+admin.site.register(Bookmark, BookmarkAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Tag2Bookmark)
