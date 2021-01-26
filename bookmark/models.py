7642bcc307ff8ccb827e1b4aa00f1fd747490257
Kensuke Nagae <kyanny@gmail.com>
2021-01-27 01:07:34 +0900

    Add moodel and migration

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Bookmark(models.Model):
+    url = models.TextField()
+    tags = models.ManyToManyField("Tag", through="Tag2Bookmark")
+
+    class Meta:
+        db_table = 'bookmark'
+
+    def __str__(self):
+        return self.url
+
+
+class Tag(models.Model):
+    name = models.TextField()
+
+    class Meta:
+        db_table = 'tag'
+
+    def __str__(self):
+        return self.name
+
+
+class Tag2Bookmark(models.Model):
+    bookmark = models.ForeignKey("Bookmark", on_delete=models.CASCADE)
+    tag = models.ForeignKey("Tag", on_delete=models.CASCADE)
+    tagged_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'tag2bookmark'
